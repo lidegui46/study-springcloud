5b0ee9c4e29c36f654b53e020f7dc279b237b75a
lidegui <lidegui46@163.com>
2018-08-13 18:00:19 +0800

    eureka

---
+++ study-springCloud-eurekaServer/src/main/java/com/ldg/study/springCloud/eurekaServer/Application.java
@@ -0,0 +1,19 @@
+package com.ldg.study.springCloud.eurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 服务 注册中心
+ *
+ * @author： ldg
+ * @create date： 2018/8/13
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
